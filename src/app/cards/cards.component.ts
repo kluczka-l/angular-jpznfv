17d2411f9d619b9f80438c4085db2af808e70bf9
kluczka-l <kenneth.luczka@lmco.com>
2019-06-26 18:45:50 -0400

    3 files modified, 1 added

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -8,13 +8,13 @@ import { Component, OnInit } from '@angular/core';
 export class CardsComponent implements OnInit {
 kard1 = { title: 'App 1 Title',
           subTitle: 'App 1 Sub Title',
-          imageText: 'App 1 Image Text',
+          imageText: 'App 1 Description',
           imageUrl: "https://github.com/kluczka-l/angular-jpznfv/blob/master/src/assets/ripples.jpg?raw=true",
           footer: "App 1 Footer" };
 
 kard2 = { title: 'App 2 Title',
           subTitle: 'App 2 Sub Title',
-          imageText: 'App 2 Image Text',
+          imageText: 'App 2 Description',
           imageUrl: "https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(97).jpg",
           footer: "App 2 Footer" };
 
