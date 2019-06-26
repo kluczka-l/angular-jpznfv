807e4fcf058847d22276e8b176062a5f2fbcde63
kluczka-l <kenneth.luczka@lmco.com>
2019-06-26 15:12:05 -0400

    6 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,6 @@ import { Component } from '@angular/core';
 export class AppComponent  {
   siteName = 'Ripples Angular';
 
-  kard1 = { title: 'Card 1 Title',
-          subTitle: 'Card 1 Sub Title',
-          imageText: 'Card 1 Image Text',
-          imageUrl: "https://github.com/kluczka-l/angular-jpznfv/blob/master/src/assets/ripples.jpg?raw=true",
-          footer: "Card 1 Footer" };
-
-  kard2 = { title: 'Card 2 Title',
-          subTitle: 'Card 2 Sub Title',
-          imageText: 'Card 2 Image Text',
-          imageUrl: "https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(97).jpg",
-          footer: "Card 2 Footer" };
+  
 
 }
