fcede6454a2e709648cc6f03d02c600b1f5fb772
kluczka-l <kenneth.luczka@lmco.com>
2019-06-27 08:48:59 -0400

    3 files modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   @Input() siteName: string;
+  sidenavOpened = true;
 
   constructor() { }
 
